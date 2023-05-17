33d3109cb8045230856289e2da92b5dcacc6758a
abarakus <abarakus@bk.ru>
2023-05-17 03:56:15 +0300

    some fixes

--- src/main/java/ru/plorum/reporter/view/IndexView.java
+++ src/main/java/ru/plorum/reporter/view/IndexView.java
@@ -3,8 +3,10 @@ package ru.plorum.reporter.view;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.security.PermitAll;
 
 
+@PermitAll
 @PageTitle("Главная")
 @Route(value = "", layout = MainView.class)
 public class IndexView extends AbstractView {
